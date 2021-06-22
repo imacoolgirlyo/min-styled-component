405954fdff0b02e9b1ccabbbb1b2c1323929e98c
Seokyung Jung <tjrud1438@naver.com>
2021-06-23 00:57:26 +0900

    docs: styled component는 style tag 를 생성해서 추가해줌

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 function App () {
   const styled = {
     div: (styles, ...values) => {
+      console.log(styles)
       const styleStrings = styles.reduce((accum, curStyle, i) => {
         accum.push(curStyle)
         if (values[i]) {
@@ -14,7 +15,10 @@ function App () {
       // styleStrings.join('')
 
       return function NewComponent (props) {
-        const className = 'abc'
+        // const className = 'abc'
+        // className을 주입해주고
+        // html style tag에 해당 style을 추가해줌
+        // https://medium.com/@_jmoller/how-does-styled-components-work-under-the-hood-28cb035d48c6
         return <div styles={styleStrings.join('')}>{props.children}</div>
       }
     }
